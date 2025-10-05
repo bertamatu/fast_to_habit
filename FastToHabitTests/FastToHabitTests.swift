cbca9937dc6f0339a510f7e3ece397634828237a
Berta Matuliauskiene <berta.matuliauskiene@gmail.com>
2025-10-05 22:50:41 +0300

    Initial Commit

---
+++ FastToHabitTests/FastToHabitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FastToHabitTests.swift
+//  FastToHabitTests
+//
+//  Created by Berta Matuliauskiene on 05/10/2025.
+//
+
+import Testing
+@testable import FastToHabit
+
+struct FastToHabitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
